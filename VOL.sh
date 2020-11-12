69537acf97475d1507ed612b3f39117a18ce1fde
streetmask <863623116@qq.com>
2020-11-12 15:26:33 +0800

    adjust

--- VOL.sh
+++ VOL.sh
@@ -1,4 +1,4 @@
-VOL="🎧[$(amixer get Master | tail -n 1 | awk '{print $5}' | tr -d '[]')$(bluetoothctl info | grep Name | awk '{print $2}')] "
+VOL="🎧[$(amixer get Master | tail -n 1 | awk '{print $4}' | tr -d '[]')$(bluetoothctl info | grep Name | awk '{print $2}')] "
 previcous=$(sed -n '4p' ~/.status)
 
 if [ "$VOL" != "$previcous" ]
